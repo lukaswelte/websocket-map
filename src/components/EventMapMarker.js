5056977f0b0a0b811cc000aca2bdff1216358e62
Adrian <adrian@blackwall.co>
2016-10-11 14:03:21 +0200

    CSS changements
    Changements of the dots (materia FF7 style)
    + card changement (have to be perfectly centered, becaue I can’t do
    that from my side… ;( )
    Add a random picture

--- src/components/EventMapMarker.js
+++ src/components/EventMapMarker.js
@@ -8,9 +8,13 @@ export default class EventMapMarker extends PureComponent {
     if (showDetails) {
       return (
         <div className="EventMapMarker-card">
-          <div className="EventMapMarker-tap"/>
-            <h1>{title}</h1>
-            <h2>{subtitle}</h2>
+            <div className="EventMapMarker-header">
+            </div>
+            <div className="EventMapMarker-content">
+              <h1>{title}</h1>
+              <h2>{subtitle}</h2>
+              <h3 style={{color:"#ff285c",}}>2.5Km from you</h3>
+            </div>
         </div>
       )
     }
