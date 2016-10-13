108dc78e3780762a3f69d1d82a37d5119bb911ae
Adrian <adrian@blackwall.co>
2016-10-13 22:06:17 +0200

    Introductions screens
    4 introductions screens done.

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Introduction.css';
 
 export default class Introduction extends Component {
   render() {
@@ -7,24 +8,55 @@ export default class Introduction extends Component {
     switch (params.id) {
       case "1":
         return (
-          <div>
-            First Page
+          <div className="Intro-background">
+            <div className="Intro-content">
+              <div className="monogramBlack"/>
+              <h1>This is you<br />& your<br />localisation</h1>
+              <div className="Intro-check"/>
+            </div>
           </div>
         );
 
       case "2":
         return (
-          <div>
-            Second Page
+          <div className="Intro-background">
+            <div className="Intro-background-dots">
+            <div className="Intro-content">
+              <div className="monogramBlack"/>
+              <h1>We will<br />inspire you<br />with little things</h1>
+              <a href="" className="Intro-check"/>
+            </div>
+            </div>
           </div>
         );
 
       case "3":
         return (
-          <div>
-            Third Page
+          <div className="Intro-background">
+            <div className="Intro-background-dots">
+              <div className="Intro-background-finger">
+                <div className="Intro-content">
+                  <div className="monogramBlack"/>
+                  <h1>Maintain<br />pressed to see<br />details</h1>
+                  <a href="" className="Intro-check"/>
+                </div>
+              </div>
+            </div>
           </div>
         );
+
+        case "4":
+          return (
+            <div className="Intro-event-card">
+                <div className="Intro-event-close">
+                  <div>X</div>
+                </div>
+                <div className="Intro-event-content">
+                  <h1>Find here<br />what the f*ck<br />is it about!</h1>
+                  <h3>Distance from you</h3>
+                </div>
+            </div>
+          );
 
       default:
         return (
