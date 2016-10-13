feb710a7a3ec879d2d835e35be354c2ef24fa42c
Adrian <adrian@blackwall.co>
2016-10-13 20:53:58 +0200

    Login CSS page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Login.css';
 
 export default class Login extends Component {
   render() {
@@ -8,22 +9,44 @@ export default class Login extends Component {
     switch (params.step) {
       case "1":
         return (
-          <div>
-            Your name?
+          <div className="Login-background">
+            <a href="" className="login-shift">Maybe later</a>
+            <div className="Login-content">
+              <div className="monogramWhite"/>
+              <div className="Login-baseline">Experience, Save & Share<br />Munich s Life</div>
+              <form className="Login-form">
+                <input type="text" className="Login-input" placeholder="I'm Dark Vador?" />
+                <input type="submit" className="Login-valid" defaultValue=""/>
+              </form>
+            </div>
           </div>
         );
 
       case "2":
         return (
-          <div>
-            Your Mail?
+          <div className="Login-background">
+            <div className="Login-content">
+              <div className="monogramWhite"/>
+              <div className="Login-baseline">Experience, Save & Share<br />Munich s Life</div>
+              <form className="Login-form">
+                <input type="text" className="Login-input" placeholder="We need your @" />
+                <input type="submit" className="Login-valid" defaultValue=""/>
+              </form>
+            </div>
           </div>
         );
 
       case "3":
         return (
-          <div>
-            Your code?
+          <div className="Login-background">
+            <div className="Login-content">
+              <div className="monogramWhite"/>
+              <div className="Login-baseline">Experience, Save & Share<br />Munich s Life</div>
+              <form className="Login-form">
+                <input type="text" className="Login-input" placeholder="Received a code?" />
+                <input type="submit" className="Login-valid" defaultValue=""/>
+              </form>
+            </div>
           </div>
         );
 
