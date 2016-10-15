4b8e44f07f5a187264b2e58c9ed98dfedf7903c5
Adrian <adrian@blackwall.co>
2016-10-15 21:27:16 +0200

    Update of Profile Page
    Problem with height, how can I remove the map behind and extand the
    high ? Could you manage that for me ?

--- src/pages/ProfilDetail.js
+++ src/pages/ProfilDetail.js
@@ -1,20 +1,21 @@
 import React, { PureComponent } from 'react';
 import { connect } from 'react-redux';
 import { goBack } from 'react-router-redux';
+import './ProfilDetail.css';
 
 class EventDetail extends PureComponent {
   render() {
     const { event, onClose } = this.props;
 
     return (
-      <div onClick={onClose} className="EventDetail-card">
-          <div className="EventDetail-close">
+      <div onClick={onClose} className="ProfilDetail-card">
+          <div className="ProfilDetail-close">
             <div>X</div>
           </div>
-          <div className="EventDetail-content">
-            <h1>Welcome Back User</h1>
-            <h2>Small line about you</h2>
-            <h3>Something else</h3>
+          <div className="ProfilDetail-monogram"/>
+          <div className="ProfilDetail-content">
+            <div className="ProfilDetail-name">Annette</div>
+            <div className="ProfilDetail-title">You are an enjoyer!</div>
           </div>
       </div>
     );
