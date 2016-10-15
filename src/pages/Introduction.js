968042ca94844589418cb4c0ce4d612a60676969
Adrian <adrian@blackwall.co>
2016-10-15 11:52:20 +0200

    Updating Introduction

--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -13,8 +13,15 @@ class Introduction extends Component {
         return (
           <div className="Intro-background">
             <div className="Intro-content">
+            <ul className="Intro-steps">
+              <li style={{backgroundColor:'#A5A5A5'}}/>
+              <li/>
+              <li/>
+              <li/>
+            </ul>
               <div className="monogramBlack"/>
-              <h1>This is you<br />& your<br />localisation</h1>
+              <h1>This is you.<br />At the center<br />of your universe.</h1>
+              <h3>We will need your localisation</h3>
               <div onClick={() => goToStep(2)} className="Intro-check"/>
             </div>
           </div>
@@ -25,8 +32,15 @@ class Introduction extends Component {
           <div className="Intro-background">
             <div className="Intro-background-dots">
             <div className="Intro-content">
+            <ul className="Intro-steps">
+              <li/>
+              <li style={{backgroundColor:'#A5A5A5'}}/>
+              <li/>
+              <li/>
+            </ul>
               <div className="monogramBlack"/>
-              <h1>We will<br />inspire you<br />with little things</h1>
+              <h1>Be inspired<br />by little things<br />around you.</h1>
+              <h3 style={{top:'50px'}}>We want to highlight you <br />the beauty of your city</h3>
               <div onClick={() => goToStep(3)} className="Intro-check"/>
             </div>
             </div>
@@ -37,26 +51,39 @@ class Introduction extends Component {
         return (
           <div className="Intro-background">
             <div className="Intro-background-dots">
-              <div className="Intro-background-finger">
                 <div className="Intro-content">
-                  <div className="monogramBlack"/>
-                  <h1>Maintain<br />pressed to see<br />details</h1>
+                <ul className="Intro-steps">
+                  <li/>
+                  <li/>
+                  <li style={{backgroundColor:'#A5A5A5'}}/>
+                  <li/>
+                </ul>
+                  <div className="starButton"/>
+                  <div className="monogramNot"/>
+                  <h1>Mark the favorite<br />spots you want<br />to remember</h1>
+                  <h3>If we missed something<br />save it for yourself :)</h3>
                   <div onClick={() => goToStep(4)} className="Intro-check"/>
                 </div>
               </div>
-            </div>
           </div>
         );
 
         case "4":
           return (
-            <div onClick={goToMap} className="Intro-event-card">
-                <div className="Intro-event-close">
-                  <div>X</div>
-                </div>
-                <div className="Intro-event-content">
-                  <h1>Find here<br />what the f*ck<br />is it about!</h1>
-                  <h3>Distance from you</h3>
+            <div onClick={goToMap} className="Intro-background">
+              <div className="Intro-background-dots">
+                  <div className="Intro-content">
+                  <ul className="Intro-steps">
+                    <li/>
+                    <li/>
+                    <li/>
+                    <li style={{backgroundColor:'#A5A5A5'}}/>
+                  </ul>
+                    <div className="monogramBlack"/>
+                    <h1>Enjoy your<br />exploration<br />& see you!</h1>
+                    <h3>Be curious, be adventurous.</h3>
+                    <div onClick={() => goToStep(4)} className="Intro-check"/>
+                  </div>
                 </div>
             </div>
           );
