20d2bb5460de15a84bab850a11c512396291364b
Adrian <adrian@blackwall.co>
2016-10-18 23:18:04 +0200

    Share event Styiling
    And deleting of useless elements.

--- src/components/ShareEventMenu.js
+++ src/components/ShareEventMenu.js
@@ -1,26 +1,30 @@
 import React, { PureComponent } from 'react';
 import Menu from 'react-motion-menu';
+import './ShareEventMenu.css';
 
 export default class ShareEventButton extends PureComponent {
   render() {
     const { style, children, onClick } = this.props;
+    var x = window.innerHeight;
+    var y = window.innerWidth;
 
     return (
       <Menu
-        customStyle={style}
+        customClass="ShareEventButton-btn"
+        customStyle={{background: 'background: url(../img/btn-Share.svg)'}}
         direction='vertical'
-        distance={80}
-        width={50}
-        height={50}
-        y={500}
-        x={100}
+        distance={-80}
+        width={60}
+        height={60}
+        y={x-100}
+        x={y/2}
         onClick={onClick}
         >
-        <i>Share Menü</i>
-        <div>FB</div>
-        <div>Twitter</div>
-        <div>G+</div>
-        {children}
+        <i></i>
+        <div className="ShareEventButton-btn-twitter"></div>
+        <div className="ShareEventButton-btn-facebook"></div>
+        <div className="ShareEventButton-btn-sms"></div>
+        <div className="ShareEventButton-btn-whatsapp"></div>
       </Menu>
     );
   }
