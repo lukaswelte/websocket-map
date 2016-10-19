c1d5a98e66ae694866ba497cca4249cc9b633916
Lukas Welte <info@lukaswelte.de>
2016-10-19 10:21:55 +0200

    styled all buttons

--- src/components/ShareEventMenu.js
+++ src/components/ShareEventMenu.js
@@ -4,27 +4,24 @@ import './ShareEventMenu.css';
 
 export default class ShareEventButton extends PureComponent {
   render() {
-    const { style, children, onClick } = this.props;
-    var x = window.innerHeight;
-    var y = window.innerWidth;
+    const x = window.innerHeight;
+    const y = window.innerWidth;
 
     return (
       <Menu
         customClass="ShareEventButton-btn"
-        customStyle={{background: 'background: url(../img/btn-Share.svg)'}}
         direction='vertical'
         distance={-80}
         width={60}
         height={60}
         y={x-100}
         x={y/2}
-        onClick={onClick}
         >
-        <i></i>
-        <div className="ShareEventButton-btn-twitter"></div>
-        <div className="ShareEventButton-btn-facebook"></div>
-        <div className="ShareEventButton-btn-sms"></div>
-        <div className="ShareEventButton-btn-whatsapp"></div>
+        <div><div className="ShareEventButton-btn-menu"/></div>
+        <div className="ShareEventButton-btn-twitter" />
+        <div className="ShareEventButton-btn-facebook" />
+        <div className="ShareEventButton-btn-sms" />
+        <div className="ShareEventButton-btn-whatsapp" />
       </Menu>
     );
   }
