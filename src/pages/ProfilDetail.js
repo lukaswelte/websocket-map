acd6d61bc8c9fe6618b98072a02c4abd70aa9245
Adrian <adrian@blackwall.co>
2016-10-27 06:21:58 -0500

    WIsh list adjustements

--- src/pages/ProfilDetail.js
+++ src/pages/ProfilDetail.js
@@ -33,7 +33,7 @@ class ProfilDetail extends PureComponent {
         </div>
 
         <div className="ProfilDetail-Wishlist">
-            <div className="ProfilDetail-category">Trips<sup>3</sup></div>
+            <div className="ProfilDetail-category">Trips</div>
             <div className="ProfilDetail-shortLine" />
             <div className="ProfilDetail-quote">The journey of a thousand miles<br/>begins with one step.</div>
             <ul className="ProfilDetail-TripsDetail">
