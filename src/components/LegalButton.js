f1f34a64c70f531ffa40c43de2aadbd889dfe969
Adrian <adrian@blackwall.co>
2016-11-13 12:22:43 +0100

    Imprint color changement

--- src/components/LegalButton.js
+++ src/components/LegalButton.js
@@ -3,10 +3,10 @@ import './LegalButton.css';
 
 export default class LegalButton extends PureComponent {
   render() {
-    const { onClick } = this.props;
+    const { onClick, color } = this.props;
 
     return (
-      <div onClick={onClick} className="LegalButton-btn">
+      <div onClick={onClick} style={{color:{color}}} className="LegalButton-btn">
       Our Legal <br />Stuffs
       </div>
     );
