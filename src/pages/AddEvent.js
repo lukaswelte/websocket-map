0a4d4b9ee08a7c694de4388dc2965b26b1d8ebc3
Adrian <adrian@blackwall.co>
2016-11-13 20:05:13 +0100

    Icon Update

--- src/pages/AddEvent.js
+++ src/pages/AddEvent.js
@@ -40,6 +40,7 @@ class AddEvent extends Component {
     return (
       <div className="AddEvent-card" style={{backgroundImage:``, backgroundSize:`cover`, backgroundPosition: 'center center'}}>
           <Helmet title="Add Event" />
+          <div className="AddEvent-icon" />
           <div onClick={onClose} className="AddEvent-close"/>
           <form onSubmit={handleSubmit} className="AddEvent-form">
             <div className="AddEvent-content">
