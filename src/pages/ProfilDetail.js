5ad9afe4238c3a9a1dc70c0f296da05848fa8846
Adrian <adrian@blackwall.co>
2016-11-16 13:03:05 +0100

    Profile Page adjustement

--- src/pages/ProfilDetail.js
+++ src/pages/ProfilDetail.js
@@ -33,11 +33,13 @@ class ProfilDetail extends PureComponent {
 
 
         <div className="ProfilDetail-onoffswitch">
-          <div className="ProfilDetail-onoffswitch-status">I'm on Map</div>
+          <div className="ProfilDetail-onoffswitch-status">I'm Visible</div>
         </div>
 
-        <div className="ProfilDetail-Subcategory">You<br />& Munich</div>
+        <div className="ProfilDetail-Subcategory">Me, I<br />& Munich</div>
         <div className="ProfilDetail-shortLine" />
+        <div className="ProfilDetail-quote">The journey of a thousand miles<br/>begins with one step.</div>
+
 
 
           <ul className="TripDetail-inspirations">
@@ -61,6 +63,22 @@ class ProfilDetail extends PureComponent {
             </li>
           </ul>
 
+
+          <div className="ProfilDetail-Subcategory">Poke Nearby<br />Friends</div>
+          <div className="ProfilDetail-shortLine" />
+          <div className="ProfilDetail-quote">Better together!</div>
+
+
+          <ul className="ProfilDetail-friends">
+            <li style={{backgroundImage:`url(http://www.planwallpaper.com/static/images/cool-pictures-for-computer-backgrounds-hd-wallpaper.jpg)`}}><div className="ProfilDetail-friends-name">Annette</div></li>
+            <li className="ProfilDetail-poked" style={{backgroundImage:`url(http://static.tumblr.com/7474abd273a041d0605a64d446d567af/thpelm6/6m5mq42zh/tumblr_static_large.jpg)`}}><div className="ProfilDetail-friends-name">Lukas</div></li>
+            <li style={{backgroundImage:`url(http://67.media.tumblr.com/de394579b7ffc341df3b10234af735ab/tumblr_mqwmwmePeG1qao4gno1_1280.jpg?.jpg)`}}><div className="ProfilDetail-friends-name">Vincent</div></li>
+            <li className="ProfilDetail-poked" style={{backgroundImage:`url(https://secure.static.tumblr.com/910705d7c2b5a8a27b8904c4b52570a1/i8r11w0/mCyndaom7/tumblr_static_tumblr_static_dpff2po0l5w0s4c0kwo8kc4s4_640.jpg)`}}><div className="ProfilDetail-friends-name">Simon</div></li>
+            <li className="ProfilDetail-poked" style={{backgroundImage:`url(http://data.whicdn.com/images/33212668/large.jpg)`}}><div className="ProfilDetail-friends-name">Jonathan</div></li>
+            <li style={{backgroundImage:`url(http://www.lovethispic.com/uploaded_images/17497-Cool-Cat.jpg)`}}><div className="ProfilDetail-friends-name">Gotty</div></li>
+            <li style={{backgroundImage:`url(http://66.media.tumblr.com/36560e675c0ffb6d50caab6768f4d35b/tumblr_nf1tafTI1K1tclds8o1_1280.jpg)`}}><div className="ProfilDetail-friends-name">Antonia</div></li>
+            <li style={{backgroundImage:`url(https://secure.static.tumblr.com/8b2cb800f4163e5f9de9cd43212078e3/cmd59g9/NP7npjbdc/tumblr_static_tumblr_static_a8q51vimf7s4o8ccsgcsww4oc_640.jpg)`}}><div className="ProfilDetail-friends-name">Johannes</div></li>
+          </ul>
 
 
             <div className="ProfilDetail-category">Trips</div>
